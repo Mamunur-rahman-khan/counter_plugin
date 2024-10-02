087e0b5d1b667e24440963060b63b54bfe82dd5e
Mamunur-rahman-khan <dipto2500@gmail.com>
2024-10-02 11:35:01 +0600

    remove extra code

--- js/script.js
+++ js/script.js
@@ -1,70 +1,3 @@
-// // Counter one start
-// let counter = document.querySelector(".counter");
-// // console.log(counter.dataset.number);
-// let x = counter.dataset.number;
-// let count = 0;
-
-
-// function counterDown(){
-//     count++;
-
-//     counter.innerHTML = count;
-
-//     if (count == x){
-//         clearInterval(stop);
-//     }
-// }
-// let stop = setInterval(function(){
-//     counterDown();
-// },8);
-// // Counter one end
-
-
-// // Counter two start
-// let countertwo = document.querySelector(".countertwo");
-// // console.log(counter.dataset.number);
-// let y = countertwo.dataset.number;
-// let counttwo = 0;
-
-
-// function counterUp(){
-//     counttwo++;
-
-//     countertwo.innerHTML = counttwo;
-
-//     if (counttwo == y){
-//         clearInterval(stoptwo);
-//     }
-// }
-// let stoptwo = setInterval(function(){
-//     counterUp();
-// },8);
-// // Counter two end
-
-
-// // Counter three start
-// let counterthree = document.querySelector(".counterthree");
-// // console.log(counter.dataset.number);
-// let z = counterthree.dataset.number;
-// let countthree = 0;
-
-
-// function counterM(){
-//     countthree++;
-
-//     counterthree.innerHTML = countthree;
-
-//     if (countthree == z){
-//         clearInterval(stopthree);
-//     }
-// }
-// let stopthree = setInterval(function(){
-//     counterM();
-// },8);
-// // Counter three end
-
-
-
 // Counter start
 let counter = document.querySelectorAll(".counter");
 let ary = Array.from(counter)
@@ -87,7 +20,6 @@ let stop = setInterval(function(){
 },1000/item.dataset.number);
 })
 // Counter end
-
 
 // Sticky header start
 $(window).scroll(function(){
